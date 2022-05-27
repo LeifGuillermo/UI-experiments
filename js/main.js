e780b871e72f55e6129886f2aa794cba7537bdf2
Leif Guillermo <guillermoleif@gmail.com>
2022-05-27 13:56:06 -0600

    Revert "Initial Commit"
    This reverts commit 20051a3fc0333b32bd1e783f429e5825e92febdf.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 listenForThemeToggle();
+addDraggableStyleSelection();
+
+function addDraggableStyleSelection() {
+	$(document).ready(function() {
+		const themeSelectionSectionElement = document.getElementById('theme-selection-section');
+		makeElementDraggable(themeSelectionSectionElement);
+	});
+}
 
 function listenForThemeToggle() {
 	$(document).ready(function() {
@@ -29,4 +37,64 @@ function changeStyleHref(stylesheet) {
 
 function changePageHeaderText(buttonValue) {
 	document.getElementById('pageHeaderTheme').innerText = buttonValue;
+}
+
+function makeElementDraggable(element) {
+	let pos1 = 0;
+	let pos2 = 0;
+	let pos3 = 0;
+	let pos4 = 0;
+
+	element.onmousedown = dragMouseDown;
+
+	function dragMouseDown(event) {
+		event = event || window.event;
+		event.preventDefault();
+
+		pos3 = event.clientX;
+		pos4 = event.clientY;
+
+		document.onmousemove = elementDrag;
+		document.onmouseup = stopMovementWhenMouseReleased;
+	}
+
+	function elementDrag(event) {
+		event = event || window.event;
+		event.preventDefault();
+		calculateNewCursorPosition(event);
+
+		if (!cursorPositionIsOutOfBounds()) {
+			setNewPositionOfElement();
+		}
+	}
+
+	function calculateNewCursorPosition(event) {
+		pos1 = pos3 - event.clientX;
+		pos2 = pos4 - event.clientY;
+		pos3 = event.clientX;
+		pos4 = event.clientY;
+	}
+
+	function cursorPositionIsOutOfBounds() {
+		const width = element.style.width;
+		const height = element.style.height;
+
+		const boundaryRight = window.width;
+		const boundaryBottom = window.height;
+
+		const newPositionY = element.offsetTop - pos2;
+		const newPositionX = element.offsetLeft - pos1;
+
+		return newPositionY < 0 || newPositionX < 0 || newPositionX > boundaryRight || newPositionY > boundaryBottom;
+	}
+
+	function setNewPositionOfElement() {
+		element.style.top = element.offsetTop - pos2 + 'px';
+		element.style.left = element.offsetLeft - pos1 + 'px';
+	}
+
+	function stopMovementWhenMouseReleased() {
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
 }
